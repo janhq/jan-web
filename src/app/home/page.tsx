296e3d096798163a72c753cd8abbf812cabf23df
Louis <louis@jan.ai>
2023-07-18 15:51:40 +0700

    chore: create next app & app route example

---
+++ src/app/home/page.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+const Page: React.FC = () => {
+  return (
+    <div className="flex text-white justify-center items-center w-screen h-screen">
+      <p>This is the Home Page</p>
+    </div>
+  );
+};
+export default Page;
