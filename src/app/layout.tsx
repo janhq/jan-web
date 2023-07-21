858c14c3152b25c38afa8500322e4a48cdc92ca8
Tony Fung <tonybka@gmail.com>
2023-07-21 17:07:59 +0800

    [JAN-824] Add AuthContext API and Authentication Buttons

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,10 +1,13 @@
 "use client";
 import "./globals.css";
 // import type { Metadata } from "next";
+import { useState } from "react";
 import { Inter } from "next/font/google";
 import { Header } from "@/components";
 import classNames from "classnames";
 import { Provider, initializeStore } from "../models/RootStore";
+import { AuthProvider } from "@/contexts/auth_context";
+import LoginModal from "@/components/Auth/LoginModal";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -15,24 +18,23 @@ const inter = Inter({ subsets: ["latin"] });
 //     "No subscription needed. Protect your privacy. Use AI without an internet connection",
 // };
 
-export default function RootLayout({
-  children,
-}: {
-  children: React.ReactNode;
-}) {
+export default function RootLayout({ children }: { children: React.ReactNode }) {
   const store = initializeStore();
+  const [showLoginModal, setShowLoginModal] = useState(false);
+
+  const toggleLoginModal = () => {
+    setShowLoginModal(!showLoginModal);
+  };
 
   return (
     <html lang="en">
-      <body
-        className={classNames(
-          inter.className,
-          "flex flex-col w-full h-full min-h-screen"
-        )}
-      >
+      <body className={classNames(inter.className, "flex flex-col w-full h-full min-h-screen")}>
         <Provider value={store}>
-          <Header />
-          {children}
+          <AuthProvider>
+            <Header handleClickLogin={toggleLoginModal} />
+            {children}
+            <LoginModal isOpen={showLoginModal} onClose={toggleLoginModal} />
+          </AuthProvider>
         </Provider>
       </body>
     </html>
--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -5,45 +5,33 @@ import { Bars3Icon, XMarkIcon } from "@heroicons/react/24/outline";
 import { DiscordIcon } from "@/assets";
 import Image from "next/image";
 import Link from "next/link";
+import { useAuth } from "@/contexts/auth_context";
 
 const navigation = [
   { name: "Chat", icon: "/icons/chat.svg", href: "/chat" },
   { name: "AIs", icon: "/icons/brain.svg", href: "/ai" },
 ];
 
-const Header: React.FC = () => {
+interface HeaderProps {
+  handleClickLogin: () => void;
+}
+
+const Header: React.FC<HeaderProps> = ({ handleClickLogin }) => {
   const [mobileMenuOpen, setMobileMenuOpen] = useState(false);
+  const { authenticated } = useAuth();
+
   return (
     <header className="text-sm bg-white border-b-[1px] border-gray-200 ">
-      <nav
-        className="mx-auto flex items-center justify-between p-6 lg:px-8"
-        aria-label="Global"
-      >
+      <nav className="mx-auto flex items-center justify-between p-6 lg:px-8" aria-label="Global">
         <div className="flex space-x-5 items-center">
           <Link href="/" className="flex items-center space-x-3">
-            <Image
-              className="h-8 w-auto"
-              src="/icons/app_icon.svg"
-              alt=""
-              width={32}
-              height={32}
-            />
+            <Image className="h-8 w-auto" src="/icons/app_icon.svg" alt="" width={32} height={32} />
             <span className="font-bold">Jan</span>
           </Link>
           {navigation.map((item) => (
-            <Link
-              key={item.name}
-              href={item.href}
-              className="text-sm font-base leading-6 text-gray-900"
-            >
+            <Link key={item.name} href={item.href} className="text-sm font-base leading-6 text-gray-900">
               <div className="flex items-center">
-                <Image
-                  src={item.icon}
-                  alt={"nav"}
-                  width={16}
-                  height={16}
-                  className="m-2"
-                />
+                <Image src={item.icon} alt={"nav"} width={16} height={16} className="m-2" />
                 <span>{item.name}</span>
               </div>
             </Link>
@@ -64,37 +52,37 @@ const Header: React.FC = () => {
             <div className="flex gap-2">
               <DiscordIcon />
             </div>
-            <button className="flex items-center gap-4">
-              <Image
-                className="rounded-sm"
-                alt="avatar"
-                src={"/icons/app_icon.svg"}
-                width={32}
-                height={32}
-              />
-              John
-            </button>
+            {authenticated ? (
+              <button className="flex items-center gap-4">
+                <Image className="rounded-sm" alt="avatar" src={"/icons/app_icon.svg"} width={32} height={32} />
+                John
+              </button>
+            ) : (
+              <div className="flex justify-center">
+                <button
+                  className="w-24 h-8 bg-gray-500 hover:bg-gray-600 text-white rounded-md mr-2"
+                  onClick={handleClickLogin}
+                >
+                  Sign up
+                </button>
+                <button
+                  className="w-24 h-8 bg-black hover:bg-gray-700 text-white rounded-md"
+                  onClick={handleClickLogin}
+                >
+                  Log in
+                </button>
+              </div>
+            )}
           </div>
         </div>
       </nav>
-      <Dialog
-        as="div"
-        className="lg:hidden"
-        open={mobileMenuOpen}
-        onClose={setMobileMenuOpen}
-      >
+      <Dialog as="div" className="lg:hidden" open={mobileMenuOpen} onClose={setMobileMenuOpen}>
         <div className="fixed inset-0 z-10" />
         <Dialog.Panel className="fixed inset-y-0 right-0 z-10 w-full overflow-y-auto bg-white px-6 py-6 sm:max-w-sm sm:ring-1 sm:ring-gray-900/10">
           <div className="flex items-center justify-between">
             <a href="#" className="-m-1.5 p-1.5">
               <span className="sr-only">Your Company</span>
-              <Image
-                className="h-8 w-auto"
-                width={32}
-                height={32}
-                src="/icons/app_icon.svg"
-                alt=""
-              />
+              <Image className="h-8 w-auto" width={32} height={32} src="/icons/app_icon.svg" alt="" />
             </a>
             <button
               type="button"
