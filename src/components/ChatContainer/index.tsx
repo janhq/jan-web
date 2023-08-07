61d6dfd0c5b439d1aa676c1bcf8f10724bf10524
Louis <louis@jan.ai>
2023-08-07 21:49:08 +0700

    chore: clean chat container

--- src/components/ChatContainer/index.tsx
+++ src/components/ChatContainer/index.tsx
@@ -99,80 +99,9 @@ const ChatContainer: React.FC<IChatContainerProps> = observer((props) => {
         setOpen={setOpen}
         onConfirmDelete={onConfirmDelete}
       />
-      <Transition.Root show={sidebarOpen} as={Fragment}>
-        <Dialog
-          as="div"
-          className="relative z-50 lg:hidden"
-          onClose={setSidebarOpen}
-        >
-          <Transition.Child
-            as={Fragment}
-            enter="transition-opacity ease-linear duration-300"
-            enterFrom="opacity-0"
-            enterTo="opacity-100"
-            leave="transition-opacity ease-linear duration-300"
-            leaveFrom="opacity-100"
-            leaveTo="opacity-0"
-          >
-            <div className="fixed inset-0 bg-gray-900/80" />
-          </Transition.Child>
-
-          <div className="fixed inset-0 flex">
-            <Transition.Child
-              as={Fragment}
-              enter="transition ease-in-out duration-300 transform"
-              enterFrom="-translate-x-full"
-              enterTo="translate-x-0"
-              leave="transition ease-in-out duration-300 transform"
-              leaveFrom="translate-x-0"
-              leaveTo="-translate-x-full"
-            >
-              <Dialog.Panel className="relative mr-16 flex w-full max-w-xs flex-1">
-                <Transition.Child
-                  as={Fragment}
-                  enter="ease-in-out duration-300"
-                  enterFrom="opacity-0"
-                  enterTo="opacity-100"
-                  leave="ease-in-out duration-300"
-                  leaveFrom="opacity-100"
-                  leaveTo="opacity-0"
-                >
-                  <div className="absolute left-full top-0 flex w-16 justify-center pt-5">
-                    <button
-                      type="button"
-                      className="-m-2.5 p-2.5"
-                      onClick={() => setSidebarOpen(false)}
-                    >
-                      <span className="sr-only">Close sidebar</span>
-                      <XMarkIcon
-                        className="h-6 w-6 text-white"
-                        aria-hidden="true"
-                      />
-                    </button>
-                  </div>
-                </Transition.Child>
-                {/* Sidebar component, swap this element with another sidebar if you like */}
-                <div className="flex grow flex-col gap-y-5 overflow-y-auto bg-white px-6 pb-4">
-                  <div className="flex h-16 shrink-0 items-center">
-                    <Image
-                      className="h-8 w-auto"
-                      src="/icons/app_icon.svg"
-                      width={32}
-                      height={32}
-                      alt="Your Company"
-                    />
-                    <span className="">Jan</span>
-                  </div>
-                  <nav className="flex flex-1 flex-col"></nav>
-                </div>
-              </Dialog.Panel>
-            </Transition.Child>
-          </div>
-        </Dialog>
-      </Transition.Root>
 
       {/* Static sidebar for desktop */}
-      <div className="hidden lg:inset-y-0 lg:flex lg:w-72 lg:flex-col overflow-hidden">
+      <div className="flex w-72 inset-y-0 flex-col overflow-hidden">
         {/* Sidebar component, swap this element with another sidebar if you like */}
         <div className="h-full flex grow flex-col gap-y-5 overflow-y-auto border-r border-gray-200 px-2">
           <div className="flex flex-col h-16 shrink-0 items-center gap-[10px] pt-1">
