26a6dfdc72f3ca9b0591ea3cbd94677a7222667a
Louis <louis@jan.ai>
2023-08-09 10:47:37 +0700

    fix: missing path import in tailwind config

--- src/screens/AIModelDetail/components/SampleImage.tsx
+++ src/screens/AIModelDetail/components/SampleImage.tsx
@@ -1,20 +1,10 @@
-import Image from "next/image";
-
 interface ISampleImageProps {
   image?: string;
 }
 export const SampleImage: React.FC<ISampleImageProps> = (props) => {
   return (
-    <div className="flex gap-[18px] w-[633px] h-[520px]">
-      <div className="w-[53px] h-full bg-[#D9D9D9] opacity-50"></div>
-      <div className="flex items-start justify-start">
-        <img
-          src={props.image}
-          alt=""
-          className="w-[77px] aspect-square"
-        />
-      </div>
-      <div className="w-[53px]  h-full bg-[#D9D9D9] opacity-50"></div>
+    <div className="flex gap-[18px] w-full">
+      <img src={props.image} alt="" className="max-w-[512px] rounded-lg overflow-hidden w-full aspect-square" />
     </div>
   );
 };
