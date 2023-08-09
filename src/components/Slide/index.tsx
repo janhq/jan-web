af11e4bea4d27b017ed260f6a036fefe555cba1e
Louis <louis@jan.ai>
2023-08-09 15:54:49 +0700

    fix: add line-clamp for text-ellipsis

--- src/components/Slide/index.tsx
+++ src/components/Slide/index.tsx
@@ -15,18 +15,20 @@ const Slide: FC<ISlideprops> = (props) => {
         src={props.image}
         alt=""
       />
-      <div className="absolute bg-[rgba(0,0,0,0.7)] w-full text-white bottom-0 rounded-bl-lg rounded-br-lg">
-        <div className="flex justify-between p-4">
-          <div className="flex flex-col">
+      <div className="absolute bg-[rgba(0,0,0,0.7)] w-full text-white bottom-0 inset-x-0 rounded-bl-lg rounded-br-lg">
+        <div className="flex flex-row justify-between items-center p-4 gap-4">
+          <div className="flex-grow flex flex-col overflow-hidden h-14 justify-center">
             <h2 className="font-bold">{props.title}</h2>
-            <span className="text-gray-300 text-xs">{props.description}</span>
+            <p className="text-gray-300 text-xs line-clamp-2 text-ellipsis">
+              {props.description}
+            </p>
           </div>
           <Link
             href={{
               pathname: `/chat`,
               query: { productId: props.productId },
             }}
-            className="flex w-40 items-center justify-center gap-2 px-5 py-[10px] rounded-md bg-white text-black"
+            className="flex-none flex w-30 h-12 items-center justify-center gap-2 px-5 py-[10px] rounded-md bg-white text-black"
           >
             Try now
           </Link>
