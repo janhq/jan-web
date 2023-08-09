7aea2f4dc90c0d772f0475f797364cfefa30e2a9
Louis <louis@jan.ai>
2023-08-09 15:35:05 +0700

    fix: AI Model Detail - slow response

--- src/app/ai/[slug]/page.tsx
+++ src/app/ai/[slug]/page.tsx
@@ -1,27 +1,11 @@
-import { Product, Section } from "@/models/Product";
-import { AIModelDetail } from "@/screens/AIModelDetail";
-import { api } from "@/services/api";
+import AIModelDetail from "@/screens/AIModelDetail";
 
 interface IModelDetailProps {
   params: { slug: string };
 }
 
 const ModelDetail: React.FC<IModelDetailProps> = async ({ params }) => {
-
-  const discover = await api.getConfigurations("discover");
-  const categoryProducts: Product[] =
-    discover.kind === "ok"
-      ? discover.configuration?.sections?.find(
-          (section: Section) => section.name === "all_categories"
-        )?.products
-      : [];
-
-  const product: Product =
-    categoryProducts
-      .flatMap((e) => e.action.params.products)
-      .filter((e): e is Product => e?.name === params.slug)[0] || {};
-
-  return <AIModelDetail product={product} />;
+  return <AIModelDetail slug={params.slug} />;
 };
 
 export default ModelDetail;
