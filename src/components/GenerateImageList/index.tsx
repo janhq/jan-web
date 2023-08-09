2197f26f2d17f138e63e52fecd31ebd687121bd4
Louis <louis@jan.ai>
2023-08-09 14:25:04 +0700

    fix: [JAN-1030] Just show all models in grid

--- src/components/GenerateImageList/index.tsx
+++ src/components/GenerateImageList/index.tsx
@@ -13,13 +13,9 @@ const GenerateImageList: React.FC<IGenerateImage> = (props) => {
           <Image src={"icons/ic_image.svg"} width={20} height={20} alt="" />
           <h2 className="text-gray-900 font-bold">Generate Images</h2>
         </div>
-        <button className="flex gap-2 px-3 py-2 bg-gray-200 text-[#1F2A37] rounded-md">
-          <span>View all</span>
-          <span>&#8594;</span>
-        </button>
       </div>
-      <div className="mt-2 grid grid-cols-2 gap-x-4 gap-y-10 sm:gap-x-6 md:grid-cols-4 md:gap-y-0 lg:gap-x-8">
-        {props.products.slice(0, 4).map((item, index) => (
+      <div className="mt-2 grid grid-cols-2 gap-4 sm:gap-6 md:grid-cols-4 lg:gap-8 wrap">
+        {props.products.map((item, index) => (
           <GenerateImageCard
             key={index}
             name={item.name}
