db4774f6d6c4fcf7e16a290674d6d63b0c12b1f7
Louis <louis@jan.ai>
2023-08-11 10:47:08 +0700

    fix: hide mock ui and highlight style

--- src/components/Auth/Profile.tsx
+++ src/components/Auth/Profile.tsx
@@ -6,6 +6,7 @@ import "react-toastify/dist/ReactToastify.css";
 import { useAuth } from "@/contexts/authContext";
 import { useStore } from "../../models/RootStore";
 import Link from "next/link";
+import { Dialog } from "@headlessui/react";
 
 interface ProfileProps {
   // Parent component can inject a function to close this modal once the close button is clicked
@@ -46,7 +47,11 @@ const Profile: React.FC<ProfileProps> = ({
   };
 
   return (
-    <div className="fixed top-0 left-0 right-0 bottom-0 bg-opacity-50 flex justify-center items-center bg-black z-50">
+    <Dialog
+      open={isOpen}
+      onClose={closeProfileFunc}
+      className="fixed top-0 left-0 right-0 bottom-0 bg-opacity-50 flex justify-center items-center bg-black z-50"
+    >
       <ToastContainer
         position="bottom-center"
         autoClose={500}
@@ -58,104 +63,105 @@ const Profile: React.FC<ProfileProps> = ({
         draggable
         pauseOnHover
       />
-      <div className="relative flex justify-center items-center flex-col w-[283px] h-[430px] bg-white p-2 rounded-lg shadow-2xl">
-        {/* Exit button */}
-        <Image
-          className="absolute top-0 right-0 mt-[5px] mr-[5px] p-2"
-          src="/icons/close_modal_btn.png"
-          alt="Close button"
-          width={28}
-          height={28}
-          priority
-          onClick={closeProfileFunc}
-        />
-        {/** Profile image */}
-        <div className="flex justify-center items-center mt-8">
-          <div className="relative">
-            <div
-              className="relative rounded-full overflow-hidden"
-              style={{ width: "60px", height: "60px" }}
-            >
-              <img
-                src={currentUser?.photoURL || "/icons/app_icon.svg"}
-                alt={"Profile Image"}
-                className="rounded-full"
-              />
-            </div>
-            <Image
+      <Dialog.Panel>
+        <div className="relative flex justify-center items-center flex-col w-[283px] bg-white p-2 rounded-lg shadow-2xl">
+          {/* Exit button */}
+          <Image
+            className="absolute top-0 right-0 mt-[5px] mr-[5px] p-2 cursor-pointer"
+            src="/icons/close_modal_btn.png"
+            alt="Close button"
+            width={28}
+            height={28}
+            priority
+            onClick={closeProfileFunc}
+          />
+          {/** Profile image */}
+          <div className="flex justify-center items-center mt-8">
+            <div className="relative">
+              <div
+                className="relative rounded-full overflow-hidden"
+                style={{ width: "60px", height: "60px" }}
+              >
+                <img
+                  src={currentUser?.photoURL || "/icons/app_icon.svg"}
+                  alt={"Profile Image"}
+                  className="rounded-full"
+                />
+              </div>
+              {/* <Image
               className="absolute bottom-0 right-0 w-1/3 h-auto"
               src="/icons/upload_photo.svg"
               alt="Edit text button"
               width={18}
               height={18}
               priority
-            />
+            /> */}
+            </div>
           </div>
-        </div>
-        {/** Name */}
-        <div className="flex flex-col justify-center items-center mt-4">
-          <div className="flex flex-row">
-            <p className="text-base mr-1">
-              {currentUser?.displayName || "Unknown Name"}
-            </p>
-            <Image
+          {/** Name */}
+          <div className="flex flex-col justify-center items-center mt-4">
+            <div className="flex flex-row">
+              <p className="text-base mr-1">
+                {currentUser?.displayName || "Unknown Name"}
+              </p>
+              {/* <Image
               src="/icons/edit_text.svg"
               alt="Edit text button"
               width={18}
               height={18}
               priority
-            />
+            /> */}
+            </div>
+            <p className="text-xs text-gray-400">
+              {currentUser?.email || "Unknown Email"}
+            </p>
           </div>
-          <p className="text-xs text-gray-400">
-            {currentUser?.email || "Unknown Email"}
-          </p>
-        </div>
-        {/** Functional editors */}
-        <div className="flex flex-col mt-10 flex-grow">
-          <div className="flex flex-row text-gray-700 items-center hover:bg-gray-100 hover:cursor-pointer">
-            <div className="w-[24px] h-[24px] bg-[#FF9500] rounded-full flex justify-center items-center">
+          {/** Functional editors */}
+          <div className="flex flex-col mt-10 flex-grow">
+            <div className="flex flex-row text-gray-500 hover:text-gray-700 items-center hover:cursor-pointer ">
+              <div className="w-[24px] h-[24px] bg-[#FF9500] rounded-full flex justify-center items-center">
+                <Image
+                  src="/icons/share_with_friend_btn.svg"
+                  alt="Share button"
+                  width={15}
+                  height={15}
+                  priority
+                />
+              </div>
+              <p className="ml-1">Tell a friend</p>
+            </div>
+            <div className="flex flex-row justify-between mt-3">
+              <input
+                ref={sharingURLTextRef}
+                type="text"
+                className="bg-gray-100 px-4 py-1 rounded placeholder-gray-500 text-gray-900 border border-gray-300 mr-1"
+                placeholder="https://jan.ai"
+                disabled
+              />
               <Image
-                src="/icons/share_with_friend_btn.svg"
-                alt="Share button"
-                width={15}
-                height={15}
+                className="hover:cursor-pointer "
+                src="/icons/copy.svg"
+                alt="Coppy button"
+                width={28}
+                height={28}
                 priority
+                onClick={copyToClipboard}
               />
             </div>
-            <p className="ml-1">Tell a friend</p>
-          </div>
-          <div className="flex flex-row justify-between mt-3">
-            <input
-              ref={sharingURLTextRef}
-              type="text"
-              className="bg-gray-100 px-4 py-1 rounded placeholder-gray-500 text-gray-900 border border-gray-300 mr-1"
-              placeholder="https://jan.ai"
-              disabled
-            />
-            <Image
-              className="hover:bg-gray-100 hover:cursor-pointer"
-              src="/icons/copy.svg"
-              alt="Coppy button"
-              width={28}
-              height={28}
-              priority
-              onClick={copyToClipboard}
-            />
-          </div>
-          <div
-            className="flex flex-row mt-3 text-gray-500 hover:bg-gray-100 hover:cursor-pointer"
-            onClick={onSignOutClick}
-          >
-            <Image
-              src="/icons/unicorn_exit.svg"
-              alt="Logout button"
-              width={20}
-              height={20}
-              priority
-            />
-            <p className="ml-1">Log out</p>
-          </div>
-          <div className="flex flex-row mt-3 text-gray-500 hover:bg-gray-100 hover:cursor-pointer">
+            <div
+              className="flex flex-row my-5 text-gray-500 hover:text-gray-700 hover:cursor-pointer"
+              onClick={onSignOutClick}
+            >
+              <Image
+                src="/icons/unicorn_exit.svg"
+                alt="Logout button"
+                width={20}
+                height={20}
+                priority
+              />
+              <p className="ml-1">Log out</p>
+            </div>
+            {/* <div className="flex flex-row mt-3 text-gray-500 hover:bg-gray-100 hover:cursor-pointer">
             <Image
               src="/icons/trash_bin.svg"
               alt="Delete button"
@@ -164,27 +170,25 @@ const Profile: React.FC<ProfileProps> = ({
               priority
             />
             <p className="ml-1">Delete Account</p>
+          </div> */}
+          </div>
+          {/** Bottom section */}
+          <div className="flex flex-row justify-between pl-4 pr-4 items-center hover:cursor-pointer">
+            <p className="text-gray-500 hover:text-gray-800">
+              <Link href="/privacy" rel="noopener noreferrer">
+                Privacy
+              </Link>
+            </p>
+            <p className="text-center font-bold text-2xl pb-3 ml-2 mr-2">.</p>
+            <p className="text-gray-500 hover:text-gray-800">
+              <Link href="/support" rel="noopener noreferrer">
+                Support
+              </Link>
+            </p>
           </div>
         </div>
-        {/** Bottom section */}
-        <div className="flex flex-row justify-between pl-4 pr-4 items-center hover:cursor-pointer">
-          <p className="text-gray-500 hover:text-gray-800">
-            <Link href="/privacy" rel="noopener noreferrer">
-              Privacy
-            </Link>
-          </p>
-          <p className="text-center font-bold text-2xl pb-3 ml-2 mr-2">.</p>
-          <p className="text-gray-500 hover:text-gray-800">
-            <Link
-              href="/support"
-              rel="noopener noreferrer"
-            >
-              Support
-            </Link>
-          </p>
-        </div>
-      </div>
-    </div>
+      </Dialog.Panel>
+    </Dialog>
   );
 };
 
--- src/components/Settings/SettingsModal.tsx
+++ src/components/Settings/SettingsModal.tsx
@@ -37,27 +37,30 @@ const SettingsModal: React.FC<SettingsModalProps> = ({
       <div className="flex flex-col absolute top-0 right-0 mt-20 mr-1 w-60 bg-white border border-gray-300 p-2 rounded-lg shadow-md">
         <Dialog.Panel>
           <div
-            className="flex flex-col p-3 hover:bg-gray-100 hover:cursor-pointer"
-            onClick={openProfileSetting}
+            className="group flex flex-col p-3 text-gray-500 hover:text-gray-700 hover:cursor-pointer"
+            onClick={() => {
+              setOpen(false);
+              openProfileSetting();
+            }}
           >
             <p className="text-lg mr-1 font-semibold">
               {currentUser?.displayName || "Unknown Name"}
             </p>
-            <p className="text-sm text-gray-400">
+            <p className="text-sm text-gray-400 group-hover:text-gray-500">
               {currentUser?.email || "Unknown Email"}
             </p>
           </div>
           {/* <div className="p-3 border-t border-gray-200 text-gray-500 hover:bg-gray-100 hover:cursor-pointer">
             <p>Account Settings</p>
           </div> */}
-          <div className="flex flex-row p-3 border-t border-gray-200 text-gray-500 hover:bg-gray-100 hover:cursor-pointer">
+          <div className="flex flex-row p-3 border-t border-gray-200 text-gray-500 hover:text-gray-700 hover:cursor-pointer">
             <img
               src="/icons/arrow-circle-down.svg"
               alt="Download icon"
               width="16"
               height="16"
             />
-            <p className="ml-1 text-gray-500">
+            <p className="ml-1">
               <a
                 href={process.env.NEXT_PUBLIC_DOWNLOAD_APP_IOS || ""}
                 target="_blank"
@@ -88,7 +91,7 @@ const SettingsModal: React.FC<SettingsModalProps> = ({
             </div>
           </div> */}
           <div className="border-t border-gray-200 text-gray-500">
-            <div className="pl-3 pt-2 pb-2 hover:bg-gray-100 hover:cursor-pointer">
+            <div className="pl-3 pt-2 pb-2 text-gray-500 hover:text-gray-700 hover:cursor-pointer">
               <Link
                 href="/privacy"
                 onClick={() => setOpen(false)}
@@ -97,7 +100,7 @@ const SettingsModal: React.FC<SettingsModalProps> = ({
                 Privacy
               </Link>
             </div>
-            <div className="pl-3 pt-2 pb-2 hover:bg-gray-100 hover:cursor-pointer">
+            <div className="pl-3 pt-2 pb-2 text-gray-500 hover:text-gray-700 hover:cursor-pointer">
               <Link
                 href="/support"
                 rel="noopener noreferrer"
@@ -108,7 +111,7 @@ const SettingsModal: React.FC<SettingsModalProps> = ({
             </div>
           </div>
           <div
-            className="pt-3 pb-1 pl-3 border-t border-gray-200 text-gray-500 hover:bg-gray-100 hover:cursor-pointer"
+            className="pt-3 pb-1 pl-3 border-t border-gray-200 text-gray-500 hover:text-gray-700 hover:cursor-pointer"
             onClick={handlSignOutClick}
           >
             <p>Sign Out</p>
