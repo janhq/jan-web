2b2a4586362fea559634d6b72ddd132a68a46e09
NamH <NamNh0122@gmail.com>
2023-08-23 19:35:38 -0700

    Fix: logout from next auth does not logout keycloak session (#74)
    Signed-off-by: James Nguyen <james@jan.ai>
    Co-authored-by: James Nguyen <james@jan.ai>

--- app/api/auth/logout/route.ts
+++ app/api/auth/logout/route.ts
@@ -1,16 +1,13 @@
 // @ts-nocheck
 import { getServerSession } from "next-auth";
 import { authOptions } from "../[...nextauth]/route";
-import { getIdToken } from "@/_utils/tokenAccessor";
 
 export async function GET() {
   const session = await getServerSession(authOptions);
   if (session) {
-    const idToken = await getIdToken();
-
-    const url = `${
-      process.env.END_SESSION_URL
-    }?id_token_hint=${idToken}& post_logout_redirect_uri=${encodeURIComponent(
+    const url = `${process.env.END_SESSION_URL}?id_token_hint=${
+      session.id_token
+    }&post_logout_redirect_uri=${encodeURIComponent(
       process.env.NEXTAUTH_URL ?? "/"
     )}`;
 
